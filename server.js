8d963f7d12d5dd5b605f2657c34abaa8738cdf3d
Chad Dombrova <chadrik@gmail.com>
2015-01-16 21:09:56 -0800

    Server modified for simple local access

--- server.js
+++ server.js
@@ -2,32 +2,17 @@
 
 var express = require('express');
 var config = require('configure');
-var twilio = require('twilio');
 
 var Buzzer = require('./lib/buzzer.js');
-var Validators = require('./lib/validators.js');
+var buzzer = new Buzzer(config);
 
-process.env.TWILIO_AUTH_TOKEN = config.twilio_token;
-
-var app = express(),
-    buzzer = new Buzzer(config, new Validators(config));
+var app = express();
 
 console.log('Starting...');
 
-app.get('/sms', twilio.webhook(), function (request, response) {
-    console.log(request.query);
-
-    var sms = request.query;
-
-    buzzer.open_door(sms.Body, sms.From, function (message) {
-        var twil_res = new twilio.TwimlResponse();
-
-        twil_res.message(message || "???");
-        console.log('"' + message + '"', sms.Body, sms.From);
-
-        response.send(twil_res);
-    });
-
+app.get('/open', function (request, response) {
+    buzzer.buzz();
+    response.send("Open Sesame!");
 });
 
 var port = Number(config.port);
